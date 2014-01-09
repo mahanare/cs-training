9c5ac905c3a5a45c0e0d2d4d683a25ec2328bfdf
harinath <harinath@careerscale.in>
2014-01-09 01:44:12 +0000

    latest changes

--- wiziq-batch3/src/in/careerscale/batch3/hari/designpatterns/Demo.java
+++ wiziq-batch3/src/in/careerscale/batch3/hari/designpatterns/Demo.java
@@ -1,5 +1,8 @@
 package in.careerscale.batch3.hari.designpatterns;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Demo {
 
 	public static void main(String[] args) {
@@ -10,6 +13,12 @@ public class Demo {
 		Singleton.getInstance();
 		Singleton.getInstance();
 		Singleton.getInstance();
+		
+		System.gc();
+		
+		List myList = new ArrayList();
+		
+	
 		
 		//Singleton object = new Singleton();
 
---
+++ wiziq-batch3/src/in/careerscale/batch3/hari/inner/demo/Demo.java
@@ -0,0 +1,13 @@
+package in.careerscale.batch3.hari.inner.demo;
+
+import in.careerscale.batch3.hari.inner.Demo.MyStaticClass;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+	
+		MyStaticClass staticObject = new MyStaticClass();
+	}
+
+}
