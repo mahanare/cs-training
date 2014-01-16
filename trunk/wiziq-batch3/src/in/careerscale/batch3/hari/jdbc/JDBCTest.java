e6d03df0dee92d737edfe6b08414c60767e2a1b0
harinath@careerscale.in <harinath@careerscale.in@32fd6a7e-35d8-314c-c421-d58890ff7900>
2014-01-16 02:01:39 +0000

    JDBC code base

---
+++ trunk/wiziq-batch3/src/in/careerscale/batch3/hari/jdbc/JDBCTest.java
@@ -0,0 +1,31 @@
+package in.careerscale.batch3.hari.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.Test;
+
+public class JDBCTest {
+	
+	
+	@Test public void JDBCTest() throws SQLException{
+		
+		//Class.forName("com.mysql.jdbc.Driver");
+		
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost/office", "root", "secret");
+	
+		Statement stmt =con.createStatement();
+		ResultSet results = stmt.executeQuery("select * from employee");
+		
+		while(results.next()){
+			System.out.println("Employee Id : " +results.getInt(1)  + " :: Employee Name " + results.getString(	"first_name") );
+		
+		}
+		
+		
+	}
+
+}
