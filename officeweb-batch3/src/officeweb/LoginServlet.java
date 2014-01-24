bf0198ecae230822ff86bc316e5be69917bb7c0b
harinath <harinath@careerscale.in>
2014-01-24 01:06:37 +0000

    login module

---
+++ officeweb-batch3/src/officeweb/LoginServlet.java
@@ -0,0 +1,59 @@
+package officeweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.careeerscale.officeweb.dao.LoginDAO;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	/**
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 String user = request.getParameter("userName");
+		 String password = request.getParameter("password");
+		 
+		 System.out.println(user + "  " +  password);
+	}
+	
+	*/
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 String user = request.getParameter("userName");
+		 String password = request.getParameter("password");
+		 
+		 LoginDAO dao = new LoginDAO();
+		 boolean result= dao.login(user, password);
+		 PrintWriter writer = response.getWriter();
+		 writer.write("Login status : "+ result);
+		 
+		 
+		 System.out.println(user + "  " +  password);
+				 
+	}
+
+}
