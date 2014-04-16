10a0be3b1430fd26df94897c1e568ca0579b2682
harinath <harinath@careerscale.in>
2014-04-16 00:13:22 +0000

    initial commit for batch 4

---
+++ batch4/basics/src/basics/HelloWorld.java
@@ -0,0 +1,20 @@
+package basics;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+	}
+	
+	public void sayHello(){
+		System.out.println("Hello");
+	}
+	
+	public void sayHi(){
+		System.out.println("Hi");
+	}
+	
+	
+}
