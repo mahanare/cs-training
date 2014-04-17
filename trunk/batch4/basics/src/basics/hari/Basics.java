d7a7a0449eebd462e584ff0cfd6aeeaab40feabe
harinath@careerscale.in <harinath@careerscale.in@32fd6a7e-35d8-314c-c421-d58890ff7900>
2014-04-17 00:45:00 +0000

    Java basics (syntax, convention, key words etc)

---
+++ trunk/batch4/basics/src/basics/hari/Basics.java
@@ -0,0 +1,33 @@
+package basics.hari;
+
+/**
+ * 
+ * 1. Keywords
+ 		http://docs.oracle.com/javase/tutorial/java/nutsandbolts/_keywords.html
+ * 2. Syntax
+ 		Declaration 
+ 		Definition
+ * 3. Naming
+		All class names should be PASCAL CASE
+ 		All variables should be CAMEL CASE
+ 		All methods should be CAMEL CASE.  
+ 		http://docs.oracle.com/javase/tutorial/java/nutsandbolts/variables.html
+ * 4. Basic structures and programming
+ * @author harinath
+ *
+ */
+public class Basics  {
+	
+	private int count =10;
+	
+
+	public void sayHello(int age, String name){
+		for(int i=0; i < 10; i++){
+			System.out.println(i);
+		}
+	}
+	
+
+	
+
+}
