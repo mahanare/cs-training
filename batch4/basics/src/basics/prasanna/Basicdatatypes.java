a0b030e93d839640c4d5bb69bd0f6977d13e52c5
prasannaj45 <prasannaj45@gmail.com>
2014-04-18 14:12:47 +0000

    minute changes on basicdatatype program

---
+++ batch4/basics/src/basics/prasanna/Basicdatatypes.java
@@ -0,0 +1,18 @@
+package basics.prasanna;
+
+public class Basicdatatypes {
+	public void byteTest () 
+	{
+		byte bytevalue = 120;
+		System.out.println("byte value is: "+ bytevalue);
+		bytevalue= (byte)130;
+		System.out.println("byte value is:"+ bytevalue);
+		
+		
+	}
+public static void main(String[] args)
+{
+Basicdatatypes data= new Basicdatatypes();
+data.byteTest();
+}
+}
