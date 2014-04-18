5f45fd421b11243a24743d6284c8dc315e05effe
harinath@careerscale.in <harinath@careerscale.in@32fd6a7e-35d8-314c-c421-d58890ff7900>
2014-04-18 00:58:28 +0000

    basic data types.

---
+++ trunk/batch4/basics/src/basics/hari/BasicDataTypes.java
@@ -0,0 +1,18 @@
+package basics.hari;
+
+public class BasicDataTypes {
+
+	public void byteTest(){
+		byte byteValue = 125;
+		System.out.println("byte value is : " + byteValue);
+		byteValue= (byte)130;
+		System.out.println("byte value is : " + byteValue);
+		
+	}
+	public static void main(String[] args) {
+		
+		BasicDataTypes data = new BasicDataTypes();
+		data.byteTest();
+	}
+
+}
