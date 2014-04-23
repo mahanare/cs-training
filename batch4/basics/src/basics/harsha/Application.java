9f85caa6d9ad4b0874aa33f4f48ecc2de9ca9c51
sriharshaboppana <sriharshaboppana@gmail.com>
2014-04-23 00:46:33 +0000

    Created Application class and HelloWorld class to demonstrate Class and Object concepts

---
+++ batch4/basics/src/basics/harsha/Application.java
@@ -0,0 +1,11 @@
+package basics.harsha;
+
+public class Application {
+	public static void main(String args[])
+	{
+		HelloWorld hw = new HelloWorld();
+		System.out.println(hw.sayHello("Harsha"));
+	}
+	
+
+}
---
+++ batch4/basics/src/basics/harsha/HelloWorld.java
@@ -0,0 +1,11 @@
+package basics.harsha;
+
+
+public class HelloWorld {
+
+	public String sayHello(String string) {
+		return "Hello " + string;
+	}
+	
+
+}
