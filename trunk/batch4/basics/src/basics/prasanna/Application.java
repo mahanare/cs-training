c963442bcd9f38c151b2ea698c252769affe13c3
prasannaj45@gmail.com <prasannaj45@gmail.com@32fd6a7e-35d8-314c-c421-d58890ff7900>
2014-04-29 23:58:20 +0000

    application for parent and child code

---
+++ trunk/batch4/basics/src/basics/prasanna/Application.java
@@ -0,0 +1,19 @@
+package basics.prasanna;
+
+import basics.prasanna.inheritance.Child;
+
+import basics.prasanna.inheritance.Parent;
+
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		
+		Child child = new Child();
+		child.sayHello("prasanna");
+		Parent p1 = new Child();
+		p1.sayHello();
+		}
+
+}
