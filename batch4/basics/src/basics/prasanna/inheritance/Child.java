3bf5b6e6ac303e70ef01236773a74e740c3988ad
prasannaj45 <prasannaj45@gmail.com>
2014-04-29 01:09:30 +0000

    inheritance

---
+++ batch4/basics/src/basics/prasanna/inheritance/Child.java
@@ -0,0 +1,15 @@
+package basics.prasanna.inheritance;
+import basics.prasanna.inheritance.Parent;
+
+public class Child extends Parent {
+	
+		
+		@Override
+		
+		public void sayHello(){
+			super.sayHello();
+			System.out.println("Hello mom and dad,");
+			
+	}
+
+}
---
+++ batch4/basics/src/basics/prasanna/inheritance/Parent.java
@@ -0,0 +1,17 @@
+package basics.prasanna.inheritance;
+
+public class Parent {
+	public void sayHello(){
+		System.out.println("Hello child");
+	}
+		public void sayHello(String name){
+		System.out.println("Hello child "+ name);
+		}
+		
+		
+		
+		
+		
+
+
+}
