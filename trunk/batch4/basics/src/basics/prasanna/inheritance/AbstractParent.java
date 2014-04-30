888f11de26974a683f3d570a51f53bdb75897a65
prasannaj45@gmail.com <prasannaj45@gmail.com@32fd6a7e-35d8-314c-c421-d58890ff7900>
2014-04-30 22:16:07 +0000

    abstract method added

---
+++ trunk/batch4/basics/src/basics/prasanna/inheritance/AbstractParent.java
@@ -0,0 +1,10 @@
+package basics.prasanna.inheritance;
+
+public abstract class AbstractParent {
+	public void sayHello()
+	{
+		System.out.println("Hi, This is Prasanna ");
+	}
+	abstract public void sayHi(String name);
+
+}
