50be9a252d44a71fe40ff92947a142ed0fbf90b1
harinath@careerscale.in <harinath@careerscale.in@32fd6a7e-35d8-314c-c421-d58890ff7900>
2014-05-06 00:25:44 +0000

    Harsha's queries answered

--- trunk/batch4/basics/src/basics/harsha/NumberCalculatorTest.java
+++ trunk/batch4/basics/src/basics/harsha/NumberCalculatorTest.java
@@ -23,10 +23,26 @@ public class NumberCalculatorTest {
 	//how should i be testing the below two functions
 	@Test
 	public void testDivide() { 
-		assertEquals(1.00,NumberCalculator.divide(1, 10),0.001);
+		double result = NumberCalculator.divide(1, 10);
+		assertEquals(0.1,result,0.001);
 	}
 	@Test
 	public void testDividebyZero() { 
-		assertEquals(error,NumberCalculator.divide(10, 0));
+		double result = NumberCalculator.divide(10, 0);
+		assertEquals(Double.POSITIVE_INFINITY, result,0.000);
+		//assertEquals(error,);
+	}
+	
+	@Test(expected=ArithmeticException.class)
+	public void testDividebyZeroForIntegers() { 
+		try{
+		int result = 10/0;
+		fail("exception should have occurred before this line");
+		}
+		catch(ArithmeticException ae){
+			
+		}
+		//assertEquals(Double.POSITIVE_INFINITY, result,0.000);
+		//assertEquals(error,);
 	}
 }
