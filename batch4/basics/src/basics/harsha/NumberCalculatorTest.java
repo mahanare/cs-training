86a3e6e3c45db248e0be6cf7e583807cbbba8fe7
sriharshaboppana <sriharshaboppana@gmail.com>
2014-05-07 23:36:43 +0000

    updated formatting.

--- batch4/basics/src/basics/harsha/NumberCalculatorTest.java
+++ batch4/basics/src/basics/harsha/NumberCalculatorTest.java
@@ -10,36 +10,38 @@ import basics.harsha.Exceptions.NumberException;
 public class NumberCalculatorTest {
 
 	@Test
-	public void testAdd() { 
-		assertEquals(0,NumberCalculator.add(0, 0));
+	public void testAdd() {
+		assertEquals(0, NumberCalculator.add(0, 0));
 	}
 
 	@Test
-	public void testSubtract() { 
-		assertEquals(-2,NumberCalculator.subtract(3, 5));
+	public void testSubtract() {
+		assertEquals(-2, NumberCalculator.subtract(3, 5));
 	}
+
 	@Test
-	public void testMultiply() throws NumberException { 
-		assertEquals(0,NumberCalculator.multiplyPositive(0, 10));
+	public void testMultiply() throws NumberException {
+		assertEquals(0, NumberCalculator.multiplyPositive(0, 10));
 	}
-	
+
 	@Test
-	public void testDivide() { 
+	public void testDivide() {
 		double result = NumberCalculator.divide(1, 10);
-		assertEquals(0.1,result,0.001);
+		assertEquals(0.1, result, 0.001);
 	}
+
 	@Test
-	public void testDividebyZero() { 
+	public void testDividebyZero() {
 		double result = NumberCalculator.divide(10, 0);
-		assertEquals(Double.POSITIVE_INFINITY, result,0.000);
-		//assertEquals(error,);
+		assertEquals(Double.POSITIVE_INFINITY, result, 0.000);
+		// assertEquals(error,);
 	}
-	
-	//how to pass this test
-	@Test(expected=DenominatorZeroException.class)
-	public void testDividebyPositiveByZero()  { 
+
+	// how to pass this test
+	@Test(expected = DenominatorZeroException.class)
+	public void testDividebyPositiveByZero() {
 		double result = NumberCalculator.dividePositive(10, 0);
-		//assertEquals(Double.POSITIVE_INFINITY, result,0.000);
-		
+		// assertEquals(Double.POSITIVE_INFINITY, result,0.000);
+
 	}
 }
