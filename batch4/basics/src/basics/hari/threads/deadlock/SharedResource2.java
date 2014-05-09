fa976811c034e292149fc7c9e31430d8ef990f9b
harinath <harinath@careerscale.in>
2014-05-09 01:05:00 +0000

    Thread deadlock example and also discussed in the class the following synchronized sleep yield wait notify notifyAll
    assignment:
    deadlock scenario and how do avoid /alternatives
    how to generate InturrpedException while thread is sleeping.
    volatile and ThreadLocal

---
+++ batch4/basics/src/basics/hari/threads/deadlock/SharedResource2.java
@@ -0,0 +1,15 @@
+package basics.hari.threads.deadlock;
+
+public class SharedResource2 {
+	
+	public synchronized void displayNumbers(int threadId){
+		
+		for(int i=10; i<20; i++){
+			System.out.println("SO2 - Thread id  : " + threadId  + " and i value : " + i);
+		   
+		}
+		
+		 
+	}
+
+}
---
+++ batch4/basics/src/basics/hari/threads/deadlock/Thread2.java
@@ -0,0 +1,39 @@
+package basics.hari.threads.deadlock;
+
+public class Thread2 extends Thread {
+
+	int id;
+	SharedResouce1 shared1;
+	SharedResource2 shared2;
+	
+	public Thread2(int id, SharedResouce1 shared1, SharedResource2 shared2){
+		this.id = id;
+		this.shared1 = shared1;
+		this.shared2 = shared2;
+	}
+	
+	@Override
+	public void run() {
+		
+		synchronized (shared2) {
+			shared2.displayNumbers(id);
+			try {
+				shared2.wait();
+			
+				Thread.yield();
+				sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//shared2.notifyAll();
+			synchronized (shared1) {
+				shared1.displayNumbers(id);
+			}
+		}
+	
+		
+	
+		
+	}
+}
