757440731fa5262e5241101015020b4fc64fbf1c
harinath <harinath@careerscale.in>
2014-05-11 01:00:36 +0000

    inner classes

---
+++ batch4/basics/src/basics/hari/inner/HelloWorldImpl.java
@@ -0,0 +1,13 @@
+package basics.hari.inner;
+
+
+public class HelloWorldImpl implements HelloWorld {
+
+	@Override
+	public void sayHello() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+}
---
+++ batch4/basics/src/basics/hari/inner/demo/Demo.java
@@ -0,0 +1,19 @@
+package basics.hari.inner.demo;
+
+import basics.hari.inner.InnerClassDemo.StaticInnerClass;
+
+
+
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+	
+		//MyStaticClass staticObject = new MyStaticClass();
+		
+		
+		StaticInnerClass staticObjectNew = new StaticInnerClass();
+	}
+
+}
