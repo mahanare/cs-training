025a23fa66da3df2cc44d4d2cc319b59e75b6b74
sriharshaboppana@gmail.com <sriharshaboppana@gmail.com@32fd6a7e-35d8-314c-c421-d58890ff7900>
2014-05-12 23:15:17 +0000

    did the assignments on deadlocks

---
+++ trunk/batch4/basics/src/harsha/baiscs/threads/deadlock/Thread2.java
@@ -0,0 +1,38 @@
+package harsha.baiscs.threads.deadlock;
+
+public class Thread2 extends Thread {
+
+	int id;
+	SharedResouce1 shared1;
+	SharedResource2 shared2;
+	
+	public Thread2(int id, SharedResouce1 shared1, SharedResource2 shared2){
+		this.id = id;
+		this.shared1 = shared1;
+		this.shared2 = shared2;
+	}
+	
+	@Override
+	public void run() {
+		
+		synchronized (shared1) {
+			shared1.displayNumbers(id);
+			try {
+				//shared1.wait();
+				sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+			//shared2.notifyAll();
+			synchronized (shared2) {
+				shared2.displayNumbers(id);
+				
+			}
+		}
+	
+		
+	
+		
+	
+}
