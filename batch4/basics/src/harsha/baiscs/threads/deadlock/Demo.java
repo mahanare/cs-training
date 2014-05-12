2b9a0a992415932c4f630f33aa3464cf159ba952
sriharshaboppana <sriharshaboppana@gmail.com>
2014-05-12 23:15:17 +0000

    did the assignments on deadlocks

---
+++ batch4/basics/src/harsha/baiscs/threads/deadlock/Demo.java
@@ -0,0 +1,20 @@
+package harsha.baiscs.threads.deadlock;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	
+		SharedResouce1 shared1 = new SharedResouce1();
+		SharedResource2 shared2 = new SharedResource2();
+		
+		Thread1 thread1 = new Thread1(1,shared1, shared2);
+		Thread2 thread2 = new Thread2(2,shared1, shared2);
+		
+		thread1.start();
+		thread1.interrupt();
+		thread2.start();
+		//System.out.println(thread1.id);
+		//System.out.println(thread1.isAlive());
+	}
+
+}
