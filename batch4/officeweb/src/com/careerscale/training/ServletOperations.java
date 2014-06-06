b45ad90803e6d1cdca75be6691228539b4c24b95
harinath <harinath@careerscale.in>
2014-06-06 00:45:38 +0000

    JSP intro

--- batch4/officeweb/src/com/careerscale/training/ServletOperations.java
+++ batch4/officeweb/src/com/careerscale/training/ServletOperations.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpSession;
  */
 public class ServletOperations extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+         
     /**
      * @see HttpServlet#HttpServlet()
      */
