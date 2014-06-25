39bcab90cfa82e8d5fd960f0988fa39db67bb71c
harinath <harinath@careerscale.in>
2014-06-25 01:12:02 +0000

    initial import

---
+++ swetha/basics/src/hello/HelloWorld.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World again");
+		
+		Numbers numbers = new Numbers();
+		
+		numbers.printNumbers();
+         
+	}
+
+}
