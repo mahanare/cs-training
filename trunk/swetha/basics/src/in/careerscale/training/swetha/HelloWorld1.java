1dc0e04e09ecef1094a6df73a0189d3424dca1b9
swetha.g25@gmail.com <swetha.g25@gmail.com@32fd6a7e-35d8-314c-c421-d58890ff7900>
2014-06-26 00:04:45 +0000

    copied files

---
+++ trunk/swetha/basics/src/in/careerscale/training/swetha/HelloWorld1.java
@@ -0,0 +1,15 @@
+package in.careerscale.training.swetha;
+
+public class HelloWorld1 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("HelloWorld!!!");
+		loops Loops= new loops();
+		Loops.forloop();
+		Loops.switchcase();
+		Loops.ifstatement();
+
+	}
+
+}
