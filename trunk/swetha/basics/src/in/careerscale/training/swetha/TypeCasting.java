727d0b2c6d844d593782a44e4710482e83cbab32
swetha.g25@gmail.com <swetha.g25@gmail.com@32fd6a7e-35d8-314c-c421-d58890ff7900>
2014-06-26 00:13:18 +0000

    format

--- trunk/swetha/basics/src/in/careerscale/training/swetha/TypeCasting.java
+++ trunk/swetha/basics/src/in/careerscale/training/swetha/TypeCasting.java
@@ -2,18 +2,17 @@ package in.careerscale.training.swetha;
 
 public class TypeCasting {
 
-	public static void main(String[] args)
-	{
-byte b;
-double d= 330.227;
-int i= 10000;
+	public static void main(String[] args) {
+		byte b;
+		double d = 330.227;
+		int i = 10000;
 
-System.out.println("conversion from d to b is");
-b= (byte) d;
-System.out.println(b);
+		System.out.println("conversion from d to b is");
+		b = (byte) d;
+		System.out.println(b);
 
-System.out.println("Conversion of i to b is");
-b= (byte) i;
-System.out.println(b);
-}
+		System.out.println("Conversion of i to b is");
+		b = (byte) i;
+		System.out.println(b);
+	}
 }
