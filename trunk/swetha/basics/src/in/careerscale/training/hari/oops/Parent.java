e607e16f6438deaebdfe7ac3f0a2f0359d099e2a
harinath@careerscale.in <harinath@careerscale.in@32fd6a7e-35d8-314c-c421-d58890ff7900>
2014-06-28 00:51:53 +0000

    inheritance and scope

--- trunk/swetha/basics/src/in/careerscale/training/hari/oops/Parent.java
+++ trunk/swetha/basics/src/in/careerscale/training/hari/oops/Parent.java
@@ -5,5 +5,21 @@ public class Parent {
 	public void sayHello(){
 		System.out.println("Hello");
 	}
+	
+	
+	protected String getLastName(){
+		System.out.println(this.getFirstName());
+		return "lastName";
+	}
+	
+	
+	private String getFirstName(){
+		return "firstName";
+	}
+	
+	String getMiddleName(){
+		return "middleName";
+	}
+	
 
 }
