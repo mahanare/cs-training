ed77ad97d7d726bdcbf9bfce210c58084025dd05
harinath <harinath@careerscale.in>
2014-06-28 00:00:32 +0000

    inheritance

---
+++ swetha/basics/src/in/careerscale/training/hari/oops/OopsDemo.java
@@ -0,0 +1,24 @@
+package in.careerscale.training.hari.oops;
+
+public class OopsDemo {
+	
+	public static void main(String[] args) {
+		
+		HelloWorld hello = new HelloWorld("Hi");
+		
+		hello = new HelloWorld("Hari","age");
+		
+		int count =5;
+		count=10;
+		
+		
+		Child child = new Child();
+		
+		
+		//java.lang.Object;
+		
+		
+	
+	}
+
+}
---
+++ swetha/basics/src/in/careerscale/training/hari/oops/Parent.java
@@ -0,0 +1,9 @@
+package in.careerscale.training.hari.oops;
+
+public class Parent {
+	
+	public void sayHello(){
+		System.out.println("Hello");
+	}
+
+}
