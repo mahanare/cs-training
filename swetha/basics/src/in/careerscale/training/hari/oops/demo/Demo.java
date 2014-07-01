821c274f2688331da51c1b204eaec79ec8f96cb6
swetha.g25 <swetha.g25@gmail.com>
2014-07-01 00:01:05 +0000

    comments made

--- swetha/basics/src/in/careerscale/training/hari/oops/demo/Demo.java
+++ swetha/basics/src/in/careerscale/training/hari/oops/demo/Demo.java
@@ -6,7 +6,8 @@ public class Demo {
 	
 	public static void main(String[] args) {
 		Parent p= new Parent();
-		
+		//only public methods can be used
+		//if this class is a child of the imported class, then protected methods can also be used.
 	}
 
 }
