7de7109e31d979c9012467e7b352a963a6d957c7
swetha.g25@gmail.com <swetha.g25@gmail.com@32fd6a7e-35d8-314c-c421-d58890ff7900>
2014-07-03 23:09:32 +0000

    Exception_example

---
+++ trunk/swetha/basics/src/in/careerscale/training/swetha/ExceptionHandling/ExceptionHandling.java
@@ -0,0 +1,30 @@
+package in.careerscale.training.swetha.ExceptionHandling;
+
+public class ExceptionHandling{
+	public void exceptionHandling(int[] a){
+		try{
+		for(int i=0;i<a.length;i++)
+			a[10]=20;
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+	}
+		public void throwsDemo(int[] b) throws ArrayIndexOutOfBoundsException{
+			for(int i=0;i<b.length;i++)
+				b[10]=20;	
+		}
+	
+	public static void main(String[] args){
+		int[] a={10,20,30,40,50,60,70,80,50};
+		int[] b={10,30,40,50,60,70,80,90,100};
+	ExceptionHandling demo=new ExceptionHandling();
+		demo.exceptionHandling(a);
+	System.out.println("hi");
+	try{
+	demo.throwsDemo(b);
+	}catch(Exception e){
+		System.out.println(e.getMessage());
+	}
+	}
+
+}
