38d6d9da3e98e789aed4ead8c6b8890cb5eb1c7b
swetha.g25 <swetha.g25@gmail.com>
2014-07-07 23:57:34 +0000

    singleton examples

---
+++ swetha/basics/src/in/careerscale/training/swetha/singleton/SingletonDemo.java
@@ -0,0 +1,9 @@
+package in.careerscale.training.swetha.singleton;
+
+public class SingletonDemo {
+public static void main(String[] args){
+	SingletonClassDemo object=new SingletonClassDemo();
+	object.getInstance();
+	
+}
+}
