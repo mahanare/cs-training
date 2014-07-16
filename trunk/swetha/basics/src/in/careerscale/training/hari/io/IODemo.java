3280976955ddde6d5c6d9c1bfdc04040135e918d
harinath@careerscale.in <harinath@careerscale.in@32fd6a7e-35d8-314c-c421-d58890ff7900>
2014-07-16 01:04:09 +0000

    latest code changes.

---
+++ trunk/swetha/basics/src/in/careerscale/training/hari/io/IODemo.java
@@ -0,0 +1,21 @@
+package in.careerscale.training.hari.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class IODemo {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine());
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println(scanner.next());
+
+	}
+
+}
