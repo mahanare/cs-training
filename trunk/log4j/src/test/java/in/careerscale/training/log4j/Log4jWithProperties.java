8d7eff54ff7c79f8c9ca97400ed76c248905ccf7
harinath@careerscale.in <harinath@careerscale.in@32fd6a7e-35d8-314c-c421-d58890ff7900>
2014-08-03 18:17:11 +0000

    log4j2

--- trunk/log4j/src/test/java/in/careerscale/training/log4j/Log4jWithProperties.java
+++ trunk/log4j/src/test/java/in/careerscale/training/log4j/Log4jWithProperties.java
@@ -1,7 +1,9 @@
 package in.careerscale.training.log4j;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 import org.junit.Test;
 
 public class Log4jWithProperties {
@@ -11,9 +13,9 @@ public class Log4jWithProperties {
   public void simpleLoggingWithStandardOutput() {
     // DOMConfigurator.configure(Loader.getResource("log4j.properties"));
     // PropertyConfigurator.configure("log4j.properties");
-    PropertyConfigurator.configure("simple_log4j.properties");
-
-    Logger log = Logger.getRootLogger();
+   // PropertyConfigurator.configure("simple_log4j.properties");
+    Configurator.initialize("console", "log4j2/xml/log4j2.xml");
+    Logger log = LogManager.getRootLogger();
     log.info("Hello this is info");
     log.warn("Hello this is warn");
     log.error("Hello this is error");
