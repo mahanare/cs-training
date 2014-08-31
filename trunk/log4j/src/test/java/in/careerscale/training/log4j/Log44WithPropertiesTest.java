f99c1a6b4bd0c502c2391307fade2ae9a0208cb7
harinath@careerscale.in <harinath@careerscale.in@32fd6a7e-35d8-314c-c421-d58890ff7900>
2014-08-31 13:09:18 +0000

    minor changes.

--- trunk/log4j/src/test/java/in/careerscale/training/log4j/Log44WithPropertiesTest.java
+++ trunk/log4j/src/test/java/in/careerscale/training/log4j/Log44WithPropertiesTest.java
@@ -9,6 +9,8 @@
  */
 package in.careerscale.training.log4j;
 
+import in.careerscale.training.PanagramChecker;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.Properties;
@@ -25,9 +27,22 @@ public class Log44WithPropertiesTest extends BaseLogTest {
 		ClassLoader loader = Thread.currentThread().getContextClassLoader();
 		URL url = loader.getResource("custom_log4j.properties");
 		PropertyConfigurator.configure(url);
-		
+		Exception e=null;
 		
 		Logger log = LogManager.getRootLogger();
+		//Multiple ways of getting logger root/by name/package/class
+		Logger loggerByName = LogManager.getLogger("application");
+		Logger loggerByPackageName = LogManager.getLogger("in.careerscale");
+		Logger loggerByClassName = Logger.getLogger(PanagramChecker.class);
+		log.info("info message");
+		log.warn("warn message");
+		log.error("error occurred", e);
+		
+		
+		loggerByClassName.info("test");
+		loggerByName.info("test");
+		loggerByPackageName.info("test");
+		
 		logPanagramResults(log);
 
 		Logger appLog = Logger.getLogger("application");
