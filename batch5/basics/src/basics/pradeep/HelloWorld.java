20f67bcc075093612dabf803f9aedc7aa4e44016
nandyala09 <nandyala09@gmail.com>
2014-11-13 18:59:30 +0000

    HelloWorld

--- batch5/basics/src/basics/pradeep/HelloWorld.java
+++ batch5/basics/src/basics/pradeep/HelloWorld.java
@@ -3,7 +3,7 @@ package basics.pradeep;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("This is my Helloworld");
 
 	}
 
