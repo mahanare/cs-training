8a6f3457dce8dbfd368fddc98f6d251836cba79c
nandyala09 <nandyala09@gmail.com>
2014-11-14 18:01:18 +0000

    BasicDatatypes

---
+++ batch5/basics/src/basics/pradeep/BasicDatatypes.java
@@ -0,0 +1,24 @@
+package basics.pradeep;
+
+//import java.lang.*;
+public class BasicDatatypes {
+	// TODO Auto-generated method stub
+
+	public static void main(String[] args)
+
+	{
+		BasicDatatypes basics = new BasicDatatypes();
+		basics.display();
+
+	}
+
+	public void display() {
+		byte Number = 120;
+		System.out.println("byteValue is :" + Number);
+		byte Number1 = (byte) 130;
+		System.out.println("byteValue is :" + Number1);
+		byte Number2 = (byte) 140;
+		System.out.println("byteValue is :" + Number2);
+
+	}
+}
