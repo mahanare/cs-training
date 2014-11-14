e0ee901d66b0f611123a2a111a1af3e7db98347b
harinath <harinath@careerscale.in>
2014-11-14 03:26:00 +0000

    Day 4 - data types

---
+++ batch5/basics/src/basics/hari/BasicDataTypes.java
@@ -0,0 +1,60 @@
+package basics.hari;
+
+/***
+ * class name should always follow PASCAL case and the variables and methods
+ * should follow CAMEL case.
+ * 
+ * @author harinath
+ * 
+ */
+public class BasicDataTypes {
+
+	private byte byteNumber;
+	private char myChar = 'A';
+
+	public void byteDemo() {
+
+		byteNumber = 120;
+		System.out.println("byte value is :" + byteNumber);
+
+		byteNumber = (byte) 130;
+		System.out.println("byte value is :" + byteNumber);
+
+		byteNumber = (byte) -140;
+		System.out.println("byte value is :" + byteNumber);
+	}
+
+	public void charDemo() {
+
+		System.out.println("Char value " + myChar);
+
+		System.out.println("int value " + (int) myChar);
+
+	}
+
+	public void stringDemo() {
+		String message = "Hello";
+
+		System.out.println("message value: " + message);
+
+		message = message + " World";
+
+		System.out.println("message value: " + message);
+
+		System.out
+				.println("byte value  is accessible in stringDemo because of scope is :"
+						+ byteNumber);
+	}
+
+	// other data types are assignments
+
+	public static void main(String[] args) {
+
+		BasicDataTypes basics = new BasicDataTypes();
+		basics.byteDemo();
+		basics.charDemo();
+		basics.stringDemo();
+
+	}
+
+}
