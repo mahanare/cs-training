6bfd55a558b2b91fb14786274877cf4bebff1c66
chaitanya.pamidi@gmail.com <chaitanya.pamidi@gmail.com@32fd6a7e-35d8-314c-c421-d58890ff7900>
2014-11-14 16:42:49 +0000

    JAVA PROGRAM ON DATA TYPES

---
+++ trunk/batch5/basics/src/basics/chaitanya/JavaBasics.java
@@ -0,0 +1,14 @@
+package basics.chaitanya;
+
+public class JavaBasics {
+	private byte bytevalue;
+	private char character;
+	private int A, B, C;
+	private float D,E,F;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
