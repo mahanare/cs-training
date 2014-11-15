73f8b5442ead77d9b7a9507458fc8902acf2c2e4
nandyala09 <nandyala09@gmail.com>
2014-11-15 02:40:23 +0000

    BasicInt

--- batch5/basics/src/basics/pradeep/BasicInt.java
+++ batch5/basics/src/basics/pradeep/BasicInt.java
@@ -5,18 +5,27 @@ public class BasicInt {
 	public static void main(String[] args) {
 		BasicInt i = new BasicInt();// object creation
 		i.teger();
+		BasicInt o = new BasicInt();
+		o.har();
 		// method calling
 		// TODO Auto-generated method stub
 	}
 
 	public void teger() {
-		int a = 8;
-		int b = 4;
-		int x = a / b;
+		int a = (byte) 8;
+		int b = (byte) 4;
+		int x = (byte) a / b;
 		System.out.println("the value of x:" + x);
 		int y = a * b % x;
 		System.out.println("the value of y:" + y);
 
+	}
+
+	public void har() {
+		char a = (byte) 108;
+		char b = (byte) 107;
+		byte z = (byte) (a + b);
+		System.out.println("the value of z:" + z);
 	}
 
 }
