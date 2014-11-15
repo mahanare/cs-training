8104e4d7ab3a6369bc6da4f0cc0d7305c85a0a48
nandyala09@gmail.com <nandyala09@gmail.com@32fd6a7e-35d8-314c-c421-d58890ff7900>
2014-11-15 00:04:08 +0000

    BasicInt

---
+++ trunk/batch5/basics/src/basics/pradeep/BasicInt.java
@@ -0,0 +1,22 @@
+package basics.pradeep;
+
+public class BasicInt {
+
+	public static void main(String[] args) {
+		BasicInt i = new BasicInt();// object creation
+		i.teger();
+		// method calling
+		// TODO Auto-generated method stub
+	}
+
+	public void teger() {
+		int a = 8;
+		int b = 4;
+		int x = a / b;
+		System.out.println("the value of x:" + x);
+		int y = a * b % x;
+		System.out.println("the value of y:" + y);
+
+	}
+
+}
