bf796b906d5f8d141684d8be1a0a08fff552a296
nandyala09 <nandyala09@gmail.com>
2014-11-18 00:00:54 +0000

    if while for loops

--- batch5/basics/src/basics/pradeep/BasicInt.java
+++ batch5/basics/src/basics/pradeep/BasicInt.java
@@ -7,6 +7,13 @@ public class BasicInt {
 		i.teger();
 		BasicInt o = new BasicInt();
 		o.har();
+		BasicInt h = new BasicInt();
+		h.ifDemo();
+		BasicInt z = new BasicInt();
+		z.forDemo();
+		BasicInt y = new BasicInt();
+		y.forDem();
+
 		// method calling
 		// TODO Auto-generated method stub
 	}
@@ -26,6 +33,35 @@ public class BasicInt {
 		char b = (byte) 107;
 		byte z = (byte) (a + b);
 		System.out.println("the value of z:" + z);
+	}
+
+	public void ifDemo() {
+		int number = 10;
+		if (number > 10) {
+			System.out.println("number is greater than 10");
+		} else if (number > 9) {
+			System.out.println("number is greater than 9 and less than 10");
+		}
+
+	}
+
+	public void forDemo() {
+		for (int i = 10; i < 19; i++) {
+			System.out.println("enter the value :" + i);
+		}
+		int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+	}
+
+	public void forDem() {
+		for (int i = 20; i <= 29; i++) {
+			System.out.println("enter the value :" + i);
+		}
+		int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		for (int i : numbers) {
+			System.out.println("enter i value from numbers");
+		}
+
 	}
 
 }
