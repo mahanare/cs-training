3cceff11be1371b6b0d0f4779b0effe576ef63c1
harinath@careerscale.in <harinath@careerscale.in@32fd6a7e-35d8-314c-c421-d58890ff7900>
2014-11-19 03:18:03 +0000

    class structure and polymorphism discussion

---
+++ trunk/batch5/basics/src/basics/hari/Demo.java
@@ -0,0 +1,14 @@
+package basics.hari;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		Calculator calc = new Calculator();
+		int result = calc.addNumbers(5, 10);
+
+		System.out.println("Sum of 5, 10 is : " + result);
+
+	}
+
+}
