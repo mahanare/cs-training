1617b9282c079af96f45fcbdcce16c428165ae84
nandyalaprashanthreddy <nandyalaprashanthreddy@gmail.com>
2014-11-21 02:30:27 +0000

    use of class frm diff package

---
+++ batch5/basics/src/basics/prashanth/PackageDemo.java
@@ -0,0 +1,11 @@
+package basics.prashanth;
+
+public class PackageDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       basics.hari.HelloWorld dem = new basics.hari.HelloWorld();
+       dem.add();
+	}
+
+}
