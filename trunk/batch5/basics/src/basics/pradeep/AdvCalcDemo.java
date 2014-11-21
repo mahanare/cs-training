08b9c3ebe3c4fa0bdf14ff510b3f1227ea69cac9
nandyala09@gmail.com <nandyala09@gmail.com@32fd6a7e-35d8-314c-c421-d58890ff7900>
2014-11-21 22:57:22 +0000

    AdvCalculator

---
+++ trunk/batch5/basics/src/basics/pradeep/AdvCalcDemo.java
@@ -0,0 +1,24 @@
+package basics.pradeep;
+
+public class AdvCalcDemo {
+
+	public AdvCalcDemo(int i, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		int i = -10;
+		int j = 20;
+		AdvCalcDemo advCalc = new AdvCalcDemo(i, j);
+		int result = advCalc.addNumbers();
+		System.out.println(" Add using AdvCalculator: " + result);
+		// TODO Auto-generated method stub
+
+	}
+
+	private int addNumbers() {
+		// TODO Auto-generated method stub
+		return (0);
+	}
+
+}
