fe0ec267903b960566385ce4a99a8a0be093aa6e
harinath <harinath@careerscale.in>
2014-12-02 03:04:31 +0000

    exception handling

--- batch5/basics/src/basics/hari/exception/Demo.java
+++ batch5/basics/src/basics/hari/exception/Demo.java
@@ -5,14 +5,20 @@ import java.io.FileNotFoundException;
 
 public class Demo {
 
-	public void demoForCheckedException() throws FileNotFoundException {
+	public void demoForCheckedException() throws Exception {
 
 		try {
 			FileInputStream file = new FileInputStream("Test");
+			System.out.println("This may never get executed");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
-			throw e;
+			// e.printStackTrace();
+			throw new Exception("File not found: demo program for exceptions",
+					e);
+		}
+
+		finally {
+			System.out.println("This gets executed always");
 		}
 
 	}
@@ -30,14 +36,32 @@ public class Demo {
 			System.out.println("Within AE block");
 			b.printStackTrace();
 			System.out.println(b.getMessage());
+		} finally {
+			System.out.println("This gets executed always");
 		}
 
 	}
 
-	public static void main(String[] args) {
+	public void demoCustomCheckedException() throws MyCheckedException {
+		System.out.println("within custom checked exception");
+		throw new MyCheckedException("this is just dummy", null);
+	}
+
+	public void demoCustomRuntmeException() {
+		System.out.println("within custom runtime exception");
+		throw new MyUnCheckedException("this never gets error by compiler");
+	}
+
+	public static void main(String[] args) throws Exception {
 
 		Demo demo = new Demo();
-		demo.demoForUncheckedException();
+		// demo.demoForUncheckedException();
+		// demo.demoForCheckedException();
+
+		// demo.demoCustomRuntmeException();
+
+		demo.demoCustomCheckedException();
+
 	}
 
 }
