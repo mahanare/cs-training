a662857989cb17f3592df1c0aabc554abf238f12
nandyala09 <nandyala09@gmail.com>
2014-12-03 23:28:10 +0000

    outer interface

---
+++ batch5/basics/src/basics/pradeep/Innerclasses/MyOuterInterface.java
@@ -0,0 +1,12 @@
+package basics.pradeep.Innerclasses;
+
+public interface MyOuterInterface {
+	public void sayHello();
+
+	public class MyInnerClass {
+		public void sayHello() {
+			System.out.println("Hello from MyInnerClass");
+		}
+	}
+
+}
