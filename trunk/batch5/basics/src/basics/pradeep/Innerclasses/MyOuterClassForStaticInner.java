81d681032c4c9961befb369e15dd5b3bb5fcaea9
nandyala09@gmail.com <nandyala09@gmail.com@32fd6a7e-35d8-314c-c421-d58890ff7900>
2014-12-03 23:27:33 +0000

    outerclass for static inner

---
+++ trunk/batch5/basics/src/basics/pradeep/Innerclasses/MyOuterClassForStaticInner.java
@@ -0,0 +1,16 @@
+package basics.pradeep.Innerclasses;
+
+public class MyOuterClassForStaticInner {
+	static class MyStaticInner {
+
+		public void sayHello() {
+			System.out.println("Hello");
+		}
+	}
+
+	public void sayHello() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
