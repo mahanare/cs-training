f247a7497ed06bf286dd9084268e665888df913e
nandyala09 <nandyala09@gmail.com>
2014-12-03 23:27:46 +0000

    my outer impplementation

---
+++ batch5/basics/src/basics/pradeep/Innerclasses/MyOuterImplementation.java
@@ -0,0 +1,11 @@
+package basics.pradeep.Innerclasses;
+
+public class MyOuterImplementation implements MyOuterInterface {
+
+	public void sayHello() {
+
+		System.out.println("Hello from MyOuterImplementation ");
+
+	}
+
+}
